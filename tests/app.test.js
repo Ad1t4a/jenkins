5d3053acc31cc5fcb0820837d2cd9c75f440bac0
aditya <adityasunilaa1@gmail.com>
2024-06-28 02:09:54 -0400

    app.test commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,24 +1,26 @@
-const request =require("supertest");
-const {app}=require("../app");
+const request = require("supertest");
+const { app } = require("../app");
+
 let server;
+
 beforeAll((done) => {
-	const PORT =process.env.TEST_PORT||3001;
-	process.env.PORT=PORT;
-	server=app.listen(PORT,()=>{
-		console.log(`Test server is running on port ${PORT}`);
-		done();
-	});
+  const PORT = process.env.TEST_PORT || 3001; // Use a different port for testing
+  process.env.PORT = PORT; // Set the process.env.PORT for the app
+  server = app.listen(PORT, () => {
+    console.log(Test server is running on port ${PORT});
+    done();
+  });
 });
-afterAll((done) =>{
-	server.close(done);
+
+afterAll((done) => {
+  server.close(done);
 });
 
 describe("GET /", () => {
-	it("should return Hello, JEnkins CI/CD with Node.js!",(done) =>{
-		request(`http://localhost: $(process.env.PORT}`)
-		.get("/")
-		.expect(200)
-		.expect("Hllo, Jenkins CI/CD with Node.js~",done):
-	});
+  it("should return Hello, Jenkins CI/CD with Node.js!", (done) => {
+    request(http://localhost:${process.env.PORT})
+      .get("/")
+      .expect(200)
+      .expect("Hello, Jenkins CI/CD with Node.js!", done);
+  });
 });
-
